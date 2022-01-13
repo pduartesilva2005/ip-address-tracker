ffc1f8ba310fd1ee0b958a90449a8dce9a74abb9
Pedro <pduartesilva2005@gmail.com>
2022-01-13 19:55:15 -0300

    Configure the project

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Rubik', sans-serif;
+    outline: none;
+
+    :root {
+      --gray900: hsl(0, 0%, 17%);
+      --gray700: hsl(0, 0%, 59%);
+    }
+  }
+`;
